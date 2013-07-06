ea424dc8690d5cb6512a62a59c927e01b42e2494
mecampbellsoup <mecampbell25@gmail.com>
2013-07-06 13:22:27 -0400

    Wrote summation & is_prime methods!

--- app_academy_practice.rb
+++ app_academy_practice.rb
@@ -2,7 +2,6 @@ def pow base, exponent
   raise 'Params must be non-negative integers' if base < 0
   raise 'Params must be non-negative integers' if exponent < 0
   
-  count = 2
   product = base
 
   if exponent == 1
@@ -14,4 +13,27 @@ def pow base, exponent
   end
 end
 
-puts pow(2,15)
+#puts pow(2,15)
+
+
+def summation array_of_numbers  #This method will take an array_of_numbers and return their sum
+  if array_of_numbers.empty?
+    return 0
+  else
+    digit = array_of_numbers.pop
+    return digit + summation(array_of_numbers)
+  end
+end
+
+puts summation([4,5,6])
+
+def is_prime? num  #takes a number num and returns true if it is prime and false otherwise.
+  start = 2
+  while true
+  start.upto(num) do
+    num % start == 0
+    start += 1
+  end
+end
+
+is_prime?(5)
