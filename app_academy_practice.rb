fadf8a610847fa9e85504f7b054502721601f0e2
mecampbellsoup <mecampbell25@gmail.com>
2013-07-07 14:53:04 -0400

    Added Ruby scripts for is_prime?, primes... those were fun!

--- app_academy_practice.rb
+++ app_academy_practice.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def pow base, exponent
   raise 'Params must be non-negative integers' if base < 0
   raise 'Params must be non-negative integers' if exponent < 0
@@ -25,15 +27,32 @@ def summation array_of_numbers  #This method will take an array_of_numbers and r
   end
 end
 
-puts summation([4,5,6])
+#puts summation([4,5,6])
 
 def is_prime? num  #takes a number num and returns true if it is prime and false otherwise.
-  start = 2
-  while true
-  start.upto(num) do
-    num % start == 0
-    start += 1
+  for test in 2..(Math.sqrt(num))
+    if (num % test) == 0
+      return false
+    end
   end
+  return true
 end
 
-is_prime?(5)
+#puts is_prime?(543)
+
+# Q4: Using your is_prime? method, write a new method, 
+# primes that takes a (non-negative, integer) number max
+# and returns an array of all prime numbers less than max. 
+
+def primes max
+  array_of_primes = []
+  for num in 2..(max)
+    if is_prime?(num)
+      array_of_primes << num
+    end
+  end
+  return array_of_primes
+end
+
+puts primes(40).join(", ")
+
