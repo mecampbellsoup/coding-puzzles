cc60608e63e7e4d8b8be805ad0875b9b5b1072c4
mecampbellsoup <mecampbell25@gmail.com>
2013-07-16 11:05:43 -0400

    Improved fibonacci methods from 1st iteration

--- fibonacci_method.rb
+++ fibonacci_method.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
-master_num = 8
+master_num = 5
 
-def gen_fib_sequence_up_to_index num
-  rec_gen_fib_values num, []
-end
+# def gen_fib_sequence_up_to_index num
+#   rec_gen_fib_values num, []
+# end
 
-def rec_gen_fib_values num, full_array
+def gen_fib_values num
   start_array = [1,1]
   count = 0
 
@@ -22,15 +22,13 @@ def rec_gen_fib_values num, full_array
   return start_array
 end
 
-p gen_fib_sequence_up_to_index master_num
+p gen_fib_values(master_num)
 
 def fib_sequence_product sequence_array
-  product = 1
-  sequence_array.each_with_index do |elem, i|
-    product = product * sequence_array[i]
-    #binding.pry
+  #product = 1
+  sequence_array.inject(1) do |product, elem|
+    product = product * elem
   end
-  product
 end
 
-p fib_sequence_product gen_fib_sequence_up_to_index master_num
+p fib_sequence_product(gen_fib_values(master_num))
