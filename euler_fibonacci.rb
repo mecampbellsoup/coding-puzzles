6979504b145ebe03e56ebe33c015339798052864
Matt Campbell <mecampbell25@gmail.com>
2013-09-29 22:28:45 -0400

    solved fibonacci, yay :)

--- euler_fibonacci.rb
+++ euler_fibonacci.rb
@@ -5,15 +5,14 @@ require 'pry'
 
 # 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 
-# By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+# By considering the terms in the Fibonacci sequence whose terminal value does not exceed four million, find the sum of the even-valued terms.
 
-def fibonacci(nth_element)
+def fibonacci(terminal_value_not_to_be_exceeded)
   first_two = [1,2]
-  (nth_element-2).times do
+  while first_two[-1] < terminal_value_not_to_be_exceeded do
     first_two << first_two[-2] + first_two[-1]
   end
-  #puts sum = first_two.each {|e|}
-  first_two
+  first_two[0, first_two.size-1]  # I want the whole thing except the last value
 end
 
 def fibonacci_sum(nth_element)
@@ -21,8 +20,8 @@ def fibonacci_sum(nth_element)
   array.inject(:+)
 end
 
-def fibonacci_sum_evens_only(nth_element)
-  array = fibonacci(nth_element)
+def fibonacci_sum_evens_only(value)
+  array = fibonacci(value)
   array_of_evens = []
   array.each do |i|
     array_of_evens << i if i.even?
@@ -30,5 +29,5 @@ def fibonacci_sum_evens_only(nth_element)
   array_of_evens.inject(:+)
 end
 
-ap fibonacci_sum(4000000)
+ap fibonacci(4000000)
 ap fibonacci_sum_evens_only(4000000)
